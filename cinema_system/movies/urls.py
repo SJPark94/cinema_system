8b83a9c5e327fd61baf4af1ec69b07449c565498
Sunjun Park <sjpark2012@gmail.com>
2018-12-02 23:14:18 -0500

    Search Functionality

--- cinema_system/movies/urls.py
+++ cinema_system/movies/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from . import views
+from paymentSystem.views import selection
 
 urlpatterns = [
     path('<str:title>', views.moviePage, name='moviePage'),
     path('sort/<str:modelType>', views.movieSort, name='movieSort'),
-
+    path('searchresult/', views.movieSearch, name='movieSearch'),
+    path('book/<str:title', views.selection, name='selectTicket'),
 ]
--- cinema_system/movies/views.py
+++ cinema_system/movies/views.py
@@ -11,3 +11,21 @@ def movieSort(request, modelType):
     print(movieResults)
     args = {'movie': movieResults, 'modelType': modelType}
     return render(request, 'movieResults.html', {'movies': movieResults, 'modelType': modelType})
+
+def movieSearch(request):
+    if request.method == 'POST':
+        print("HELLO WORLD")
+        searchString = request.POST.get("search", '')
+        if len(searchString) == 0:
+            movieObject = MovieInfo.objects.all()
+            return render(request, 'homepage/index.html', {'movies': movieObject})
+        else:
+            searchResults = MovieInfo.objects.filter(title__icontains=searchString)
+            if searchResults:
+                print("YOLO")
+                return render(request, 'searchResults.html', {'movies': searchResults})
+            else:
+                return render(request, 'nomovies.html')
+    else:
+        movieObject = MovieInfo.objects.all()
+        return render(request, 'homepage/index.html', {'movies': movieObject})
