cac1d4acf20e9b85c8f5b22cdca4891ef63725d3
Sunjun Park <sjpark2012@gmail.com>
2018-12-02 23:17:03 -0500

    Search Functionality 2.0

--- cinema_system/cinema/urls.py
+++ cinema_system/cinema/urls.py
@@ -1,7 +1,5 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.views.generic.base import TemplateView
-
 
 urlpatterns = (
     path('', include('userAccount.urls')),
@@ -9,4 +7,6 @@ urlpatterns = (
     path('accounts/', include('userAccount.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
     path('movies/', include('movies.urls')),
+    path('movies/', include('paymentSystem.urls')),
+
 )
